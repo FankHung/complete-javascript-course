a3860659145a855d16aefc3b0f656e23a401d294
Fank Hung <noctis.hung@gmail.com>
2025-08-06 00:01:38 +0800

    [FEATURE] Finish No.211

--- 13-Advanced-DOM-Bankist/starter_fank/script.js
+++ 13-Advanced-DOM-Bankist/starter_fank/script.js
@@ -482,14 +482,35 @@ headerObserver.observe(header);
 
 ///////////////////////////////////////
 // Reveal sections
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
+//   console.log(entry);
+
+//   if (!entry.isIntersecting) return;
+
+//   entry.target.classList.remove('section--hidden');
+//   observer.unobserve(entry.target);
+// };
+
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.15,
+// });
+
+// allSections.forEach(function (section) {
+//   sectionObserver.observe(section);
+//   section.classList.add('section--hidden');
+// });
+
+///////////////////////////////////////
+// Fixing a Small Scrolling Bug in "Reveal sections"
 const revealSection = function (entries, observer) {
-  const [entry] = entries;
-  console.log(entry);
-
-  if (!entry.isIntersecting) return;
-
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+  console.log(entries);
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
