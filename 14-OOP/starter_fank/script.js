c0685d0016c8684e16284ced6786f6d7265b7275
Fank Hung <noctis.hung@gmail.com>
2025-08-11 01:36:24 +0800

    [FEATURE] Finish No.221

--- 14-OOP/starter_fank/script.js
+++ 14-OOP/starter_fank/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//////////////////////////////////////////////
+// Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // 永遠不應該在構造函式內建立方法
+  // 這是因為我們有可能會使用一個構造函式來建立很多物件實例,
+  // 假設建立了1000個, 那麼也就代表構造函式內的方法被建了1000個副本, 而這個影響對代碼的效能很糟糕.
+  //   this.calcAge = function () {
+  //     console.log(2025 - this.birthYear);
+  //   };
+};
+
+const fank = new Person('Fank', 1989);
+console.log(fank);
+console.log(fank instanceof Person); // true
