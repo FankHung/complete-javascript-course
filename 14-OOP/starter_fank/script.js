a1b6ca0a746d8ab06ed233dac9cda97eee7ca789
Fank <fank@Fank-MBA25.local>
2025-08-13 01:33:08 +0800

    [FEATURE] Finish No.224

--- 14-OOP/starter_fank/script.js
+++ 14-OOP/starter_fank/script.js
@@ -44,3 +44,26 @@ console.log(fank.species, matilda.species);
 
 console.log(fank.hasOwnProperty('firstName')); // true
 console.log(fank.hasOwnProperty('species')); // false
+
+//////////////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(fank.__proto__); // Prototype of Person
+console.log(fank.__proto__.__proto__); // Prototype of Object
+console.log(fank.__proto__.__proto__.__proto__); // null
+
+// JS類的原型中的 constructor 屬性會返回此類的構造函式
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__); // Prototype of Object
+
+// 可以對 JS 內建的 Array 類新增原型方法或屬性, 並且所有的 Array 物件也都會繼承
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+const h1 = document.querySelector('h1');
+console.dir(h1);
+console.dir(x => x + 1);
